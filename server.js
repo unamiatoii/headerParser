ce3912b9755a983d258027f298f54f75c4761075
unamiatoii <pierrejaphetkonan@gmail.com>
2024-05-29 22:02:06 +0000

    first

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const os = require('os');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Fonction pour obtenir l'adresse IP locale
+const getLocalIpAddress = () => {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+};
+
+app.listen(port, () => {
+    const ipAddress = getLocalIpAddress();
+    console.log(`Serveur en écoute sur http://${ipAddress}:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.get('/whoami', function(req, res){
+    res.json({ipaddress: req.ip, language: req.headers['accept-language'], sfotware: req.headers['user-agent']})
+})
+
+module.exports = app;
